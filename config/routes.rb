ba01c7e5d155f0d4387c7ce4995fff72e26182ae
alessandrostein <alessandro@alessandrostein.com>
2018-08-23 19:22:18 -0300

    Routers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "robot#index"
+  get '/play', to: 'robot#play'
+  post '/play', to: 'robot#run'
 end
