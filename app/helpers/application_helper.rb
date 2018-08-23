e92189cfe8ca785b9a308d54de0fa0255fb4e231
alessandrostein <alessandro@alessandrostein.com>
2018-08-23 19:23:12 -0300

    Flash message helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def flash_message
+    messages = ""
+    [:warn, :success, :error].each {|type|
+      if flash[type]
+        messages += "<p class=\"message #{type}\">#{flash[type]}</p>"
+      end
+    }
+    messages.html_safe
+  end
+
 end
